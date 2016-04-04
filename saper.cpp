e7fbdd742f63ff8249089cfd01ba72cd0cfd9ba4
DeyMass <silkin.mixail@yandex.ru>
2016-04-04 17:33:15 +0700

    Let's start

---
+++ saper.cpp
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <graphics.h>
+void pole(int a, int b){
+     initwindow(1,1);
+     }
+
+int main(){
+initwindow(700,400);
+outtextxy(1,1,"Width: ");
+outtextxy(1,100,"Height: ");
+outtextxy(1,200,"Difficulty: ");
+
+
+}
